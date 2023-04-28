b29ed9b5e8622a894f342e5b20ec1be20ed2c571
Keenwah Ryce <keenwah@admins-iMac.local>
2023-04-28 15:39:14 -0500

    Added logs

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-const { getUnitedData } = require("./united")
-const { addMerchs } = require("./addToDB")
-const { getDb, connectToDb } = require("./db")
-const fs = require('fs/promises')
+const { getUnitedData } = require("./united");
+const { addMerchs } = require("./addToDB");
+const { getDb, connectToDb } = require("./db");
 
 const main = async (db) => {
-    const data = await getUnitedData()
-    await addMerchs(data, db)
-    //await fs.appendFile('message.txt', `main in index ran at ${Date.now()} \n`, err => (err) ? console.log(err) : console.log('worked'))
-    process.exit()
-}
-
+  const data = await getUnitedData();
+  await addMerchs(data, db);
+  process.exit();
+};
 
 // db connection
-let db
+let db;
 
 connectToDb((err) => {
-    if (!err) {
-        db = getDb()
-        console.log('Connected to Database')
-        main(db)
-
-    }
-
-})
-
- 
-
-
-
-
+  if (!err) {
+    db = getDb();
+    console.log("Connected to Database");
+    main(db);
+  }
+});
--- united.js
+++ united.js
@@ -1,78 +1,90 @@
-const puppeteer = require("puppeteer")
+const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
 
 const URL = "https://shopping.mileageplus.com/b____.htm";
 
 async function getUnitedData() {
-
-  const browser = await puppeteer.launch({ headless: true,
-	  				   ignoreDefaultArgs: [' --disable-extensions '],
-	  				   args: [
-						     "--disabled-gpu",
-						     "--disabled-dev-shm-usage",
-						     "--disabled-setuid-sandbox",
-						     "--no-sandbox",
-					   ],
-  					 })
-  const page = await browser.newPage()
-  await page.goto(URL, { waitUntil: "load", timeout: 0 })
+  const browser = await puppeteer.launch({
+    headless: true,
+    ignoreDefaultArgs: [" --disable-extensions "],
+    args: [
+      "--disabled-gpu",
+      "--disabled-dev-shm-usage",
+      "--disabled-setuid-sandbox",
+      "--no-sandbox",
+    ],
+  });
+  const page = await browser.newPage();
+  await page.goto(URL, { waitUntil: "load", timeout: 0 });
 
   const browserData = await page.evaluate(() => {
+    const re = /(\d+)\s(miles?\/?\$?)/;
+    const instoreUnitRe = /Earn\s(.+?)\s/;
 
-    const re = /(\d+)\s(miles?\/?\$?)/
-    const instoreUnitRe = /Earn\s(.+?)\s/
+    const myObj = [];
 
-    const myObj = []
-
-    const blockLength = document.querySelectorAll("div.mn_groupsWrap[data-sort-type='byAlpha'] li").length
+    const blockLength = document.querySelectorAll(
+      "div.mn_groupsWrap[data-sort-type='byAlpha'] li"
+    ).length;
 
     for (let i = 0; i < blockLength; i++) {
+      const date = new Date().toString();
+      const block = document.querySelectorAll(
+        "div.mn_groupsWrap[data-sort-type='byAlpha'] li"
+      )[i];
+      const merchName = block.querySelector("span.mn_merchName").innerText;
+      const link = block.querySelector("a").href;
 
-      const date = (new Date()).toString()
-      const block = document.querySelectorAll("div.mn_groupsWrap[data-sort-type='byAlpha'] li")[i]
-      const merchName = block.querySelector("span.mn_merchName").innerText
-      const link = block.querySelector("a").href
+      let price, unit, instoreIcon;
 
-      let price, unit, instoreIcon
-       
       if (block.querySelector("div.mn_merchListInstoreIcon")) {
-         price = block.querySelector("span.mn_instoreRebateDisplayRate").innerText.match(instoreUnitRe)[1]
-         unit = block.querySelector("span.mn_instoreRebateCurrency").innerText
-         instoreIcon = true
+        price = block
+          .querySelector("span.mn_instoreRebateDisplayRate")
+          .innerText.match(instoreUnitRe)[1];
+        unit = block.querySelector("span.mn_instoreRebateCurrency").innerText;
+        instoreIcon = true;
       } else if (block.querySelector("span.mn_deactivatedRebate")) {
-         price = null
-         unit = null
-         instoreIcon = false
-      } else    {
-         price = block.querySelector("span.mn_rebateValueWithCurrency").innerText.match(re)[1]
-         unit = block.querySelector("span.mn_rebateValueWithCurrency").innerText.match(re)[2]
-         instoreIcon = false
+        price = null;
+        unit = null;
+        instoreIcon = false;
+      } else {
+        price = block
+          .querySelector("span.mn_rebateValueWithCurrency")
+          .innerText.match(re)[1];
+        unit = block
+          .querySelector("span.mn_rebateValueWithCurrency")
+          .innerText.match(re)[2];
+        instoreIcon = false;
       }
 
-      myObj.push({ 
+      myObj.push({
         merchName,
         link,
         price,
         unit,
         instoreIcon,
-      })
+      });
     }
-   
-    return myObj
-  })
-  
-  const date = (new Date()).toString()
 
-  browserData.push({ date: date })
+    return myObj;
+  });
 
-  await fs.writeFile(`./logs/unitedData - ${date}.json`, JSON.stringify(browserData))     // Run this code and block line 64 to write to a file
+  const date = new Date().toString();
 
-  await browser.close()
+  browserData.push({ date: date });
 
-  return browserData        // Run this code and block line 60 to return directly into other file
+  await fs.writeFile(
+    `./logs/unitedData - ${date}.json`,
+    JSON.stringify(browserData)
+  ); // Run this code and block line 64 to write to a file
+
+  await browser.close();
+
+  return browserData; // Run this code and block line 60 to return directly into other file
 }
 
+//getUnitedData()
 
 module.exports = {
-    getUnitedData,
-}
+  getUnitedData,
+};
